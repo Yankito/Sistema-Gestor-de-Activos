bf4ffcc24ce4aec3eefe5f08a1231e3ca6623fa7
Yanko <yacuna20@alumnos.utalca.cl>
2025-01-22 16:35:25 -0300

    ultimos arreglos

--- sqat/resources/views/layouts/app.blade.php
+++ sqat/resources/views/layouts/app.blade.php
@@ -168,6 +168,7 @@
             </div>
             <!-- /.sidebar -->
         </aside>
+
         <!-- Aquí se mostrarán las vistas dinámicas -->
         <div class="content-wrapper">
             @yield('content')
@@ -226,5 +227,20 @@
         <script src="vendor/adminlte/dist/js/demo.js"></script>
         <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
         <script src="vendor/adminlte/dist/js/pages/dashboard.js"></script>
+
+        <!-- DataTables  & Plugins -->
+        <script src="vendor/adminlte/plugins/datatables/jquery.dataTables.min.js"></script>
+        <script src="vendor/adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+        <script src="vendor/adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+        <script src="vendor/adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+        <script src="vendor/adminlte/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+        <script src="vendor/adminlte/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+        <script src="vendor/adminlte/plugins/jszip/jszip.min.js"></script>
+        <script src="vendor/adminlte/plugins/pdfmake/pdfmake.min.js"></script>
+        <script src="vendor/adminlte/plugins/pdfmake/vfs_fonts.js"></script>
+        <script src="vendor/adminlte/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+        <script src="vendor/adminlte/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+        <script src="vendor/adminlte/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+
 </body>
 </html>
