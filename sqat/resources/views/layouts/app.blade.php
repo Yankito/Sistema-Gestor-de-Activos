ef9df613c24613807c53b6ce8abb0e9018b6f011
Rodrigo Dominguez <rdominguez19@alumnos.utalca.cl>
2025-01-23 11:35:36 -0300

    detallitos

--- sqat/resources/views/layouts/app.blade.php
+++ sqat/resources/views/layouts/app.blade.php
@@ -103,70 +103,91 @@
 
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary">
-            <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
-            <img src="pictures/Logo Empresas Iansa.png" alt="AdminLTE Logo" style="opacity: .8" width="200" height="auto">
-            </a>
-            <!-- Sidebar -->
-            <div>
-            <!-- Sidebar user panel (optional) -->
-            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                <div class="image">
-                <img src="pictures/perfil.png" class="img-circle elevation-2" alt="User Image">
+                <!-- Brand Logo lleva al dashboard -->
+                <a href="/dashboard" class="brand-link">
+                <img src="pictures/Logo Empresas Iansa.png" alt="AdminLTE Logo"  width="200" height="auto"  style="opacity: .8">
+                </a>
+                <!-- Sidebar -->  
+            <div class="sidebar">  
+                <!-- Sidebar user panel (optional) -->
+                <div class="user-panel mt-2 pb-2 mb-2 d-flex">
+                    <div class="image"> 
+                    <img src="pictures/perfil.png" class="img-circle elevation-2" alt="User Image">
+                    </div>
+                    <div class="info">
+                    <a href="#" class="d-block">{{ $user->nombres }}</a>
+                    </div>
                 </div>
-                <div class="info">
-                <a href="#" class="d-block">{{ $user->nombres }}</a>
+                <!-- SidebarSearch Form -->
+                <div class="form-inline" style="margin-right: 10px;">
+                    <div class="input-group" data-widget="sidebar-search">
+                    <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+                    <div class="input-group-append">
+                        <button class="btn btn-sidebar">
+                        <i class="fas fa-search fa-fw"></i>
+                        </button>
+                    </div>
+                    </div>
                 </div>
-            </div>
 
+                <!-- Sidebar Menu -->
+                <nav class="mt-2" style="margin-right: 10px;">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                    <!-- Add icons to the links using the .nav-icon class
+                        with font-awesome or any other icon font library -->
+                    <li class="nav-item">
+                        <a href="#" class="nav-link active">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>
+                            Menú 
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                        </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="/dashboard" class="nav-link">Inicio</a>
+                            </ul>
 
-            <!-- SidebarSearch Form -->
-            <div class="form-inline">
-                <div class="input-group" data-widget="sidebar-search">
-                <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-                <div class="input-group-append">
-                    <button class="btn btn-sidebar">
-                    <i class="fas fa-search fa-fw"></i>
-                    </button>
-                </div>
-                </div>
-            </div>
-
-            <!-- Sidebar Menu -->
-            <nav class="mt-2">
-                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <!-- Add icons to the links using the .nav-icon class
-                    with font-awesome or any other icon font library -->
-                <li class="nav-item menu-open">
-                    <a href="#" class="nav-link active">
-                    <i class="nav-icon fas fa-tachometer-alt"></i>
-                    <p>
-                        Menú
-                        <i class="right fas fa-angle-left"></i>
-                    </p>
-                    </a>
-                        <ul class="nav nav-treeview">
-                            <li class="nav-item">
-                                <a href="/dashboard" class="nav-link">Inicio</a>
-                        </ul>
-
-                        @if($user->esAdministrador)
+                            @if($user->esAdministrador)
+                                <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="/register" class="nav-link">Registrar Admin</a>
+                                </ul>
+                            @endif
+                        <!-- tablas -->
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-table"></i>
+                            <p>
+                                Tablas
+                                <i class="fas fa-angle-left right"></i>
+                            </p>
+                            </a>
                             <ul class="nav nav-treeview">
                             <li class="nav-item">
-                                <a href="/register" class="nav-link">Registrar Admin</a>
+                                <a href="/tablaActivos" class="nav-link">
+                                <p>Activos</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/tablaPersonas" class="nav-link">
+                                <p>Personas</p>
+                                </a>
+                            </li>
                             </ul>
-                        @endif
-                    <ul class="nav-item d-none d-sm-inline-block">
-                        <form action="/logout" method="POST" class= "d-inline">
-                            @csrf
-                            <button type="submit" class="btn btn-link nav-link">Cerrar sesión</button>
-                        </for>
+                            <ul class="nav-item d-none d-sm-inline-block">
+                            <form action="/logout" method="POST" class= "d-inline">
+                                @csrf
+                                <button type="submit" class="btn btn-link nav-link"> <i class="fas fa-sign-out-alt"> Cerrar sesión</i></button>
+                            </for>
+                            </ul>
+                        </li>
+                    </li> 
                     </ul>
-                </li>
-            </nav>
-            <!-- /.sidebar-menu -->
+                </nav>
+                <!-- /.sidebar-menu -->
+                <!-- /.sidebar -->
             </div>
-            <!-- /.sidebar -->
         </aside>
 
         <!-- Aquí se mostrarán las vistas dinámicas -->
