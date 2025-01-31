22dcc887c1a896fb3f1781ff01bd33ad11441d52
Yanko <yacuna20@alumnos.utalca.cl>
2025-01-31 17:52:40 -0300

    arreglo tabla datos

--- sqat/app/Http/Controllers/tablaDatosController.php
+++ sqat/app/Http/Controllers/tablaDatosController.php
@@ -10,10 +10,14 @@ class TablaDatosController extends Controller
     {
         $datos = DB::table('personas')
             ->leftJoin('activos', 'personas.id', '=', 'activos.usuarioDeActivo')
+            ->leftJoin('personas as usuario', 'activos.usuarioDeActivo', '=', 'usuario.id')
+            ->leftJoin('personas as responsable', 'activos.responsableDeActivo', '=', 'responsable.id')
             ->leftJoin('ubicaciones',DB::raw('COALESCE(activos.ubicacion, personas.ubicacion)'), '=', 'ubicaciones.id')
             ->select(
                 'personas.*',
                 'activos.*',
+                'usuario.rut as rutUsuario',
+                'responsable.rut as rutResponsable',
                 'ubicaciones.sitio',
                 'ubicaciones.soporteTI'
             )
