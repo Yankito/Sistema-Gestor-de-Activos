bd38667cd197aa224ff0080ca655775fca9779b2
Yanko <yacuna20@alumnos.utalca.cl>
2025-01-31 17:42:43 -0300

    arreglo tablas activos

--- sqat/app/Http/Controllers/TablaActivosController.php
+++ sqat/app/Http/Controllers/TablaActivosController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 use App\Models\Activo;
+use Illuminate\Support\Facades\DB;
 
 class TablaActivosController extends Controller
 {
     public function index()
     {
-        $activos = Activo::all();
-        return view('tablaActivos', compact('activos'));
+        $datos = DB::table('activos')
+            ->leftJoin('personas as usuario', 'activos.usuarioDeActivo', '=', 'usuario.id')
+            ->leftJoin('personas as responsable', 'activos.responsableDeActivo', '=', 'responsable.id')
+            ->leftJoin('ubicaciones', DB::raw('COALESCE(usuario.ubicacion, activos.ubicacion)'), '=', 'ubicaciones.id')
+            ->select(
+                'activos.*',
+                'usuario.rut as rutUsuario',
+                'responsable.rut as rutResponsable',
+                'ubicaciones.sitio',
+                'ubicaciones.soporteTI'
+            )
+            ->get();
+
+        return view('tablaActivos', compact('datos'));
     }
 }
