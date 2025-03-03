2f03ab4c38bebe5817a2af4633973f29af66e9e2
Yanko <yacuna20@alumnos.utalca.cl>
2025-03-03 10:03:54 -0300

    a

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://sistema-gestor-de-activos-87080374122.us-central1.run.app'),
 
     /*
     |--------------------------------------------------------------------------
