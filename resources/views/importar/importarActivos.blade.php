6c5c31194b442243d0adb8cc91163865eb53c933
Rodrigo Dominguez <rdominguez19@alumnos.utalca.cl>
2025-03-12 08:54:52 -0300

    validar el tamaño de excel

--- resources/views/importar/importarActivos.blade.php
+++ resources/views/importar/importarActivos.blade.php
@@ -30,6 +30,18 @@
                                     </button>
                                 </div>
                             @endif
+                            @if($errors->any())
+                                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                    <ul>
+                                        @foreach($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                            @endif
 
                             <!-- Excel Import Form -->
                             <form id="importForm" action="{{ route('importar.excel.activos') }}" method="POST" enctype="multipart/form-data">
